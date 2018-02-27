8eca4b3470bba85dea2ab8e099e33cc51e34799a
Alvaro <Alvaro@DESKTOP-6F2TT1K>
2018-02-27 10:03:27 +0100

    Prueba git

--- EjerciciosJavaGithub/src/ejerciciosjavagithub/EjerciciosJavaGithub.java
+++ EjerciciosJavaGithub/src/ejerciciosjavagithub/EjerciciosJavaGithub.java
@@ -15,7 +15,7 @@ public class EjerciciosJavaGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println("El valor es "+i);
             
         }
