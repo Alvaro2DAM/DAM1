bfcfa9e2c2144b2a28cecccda0619705947e257c
Alvaro <Alvaro@DESKTOP-6F2TT1K>
2018-02-27 09:49:03 +0100

    Prueba git

---
+++ EjerciciosJavaGithub/src/ejerciciosjavagithub/EjerciciosJavaGithub.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjavagithub;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class EjerciciosJavaGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("El valor es "+i);
+            
+        }
+    }
+    
+}
